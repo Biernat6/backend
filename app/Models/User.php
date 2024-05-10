f1295319414378ad8acf4baa0b1d01d8d9e79533
Biernat6 <160399356+Biernat6@users.noreply.github.com>
2024-05-10 10:41:41 +0200

    Cors update

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,11 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens; 
+
+
 class User extends Authenticatable{
-    use hasFactory, Notifiable;
+    use hasApiToken, hasFactory, Notifiable;
     
     protected $table = 'users';
     protected $primaryKey = 'user_id';
